0b58eaaa99847c90efdecbcd6b99e3b09a2283dd
Aykut Arslantaş <37196076+aykutarslantas@users.noreply.github.com>
2023-04-24 15:17:30 +0300

    Update Main.java

--- src/com/DAOCRUD/Main.java
+++ src/com/DAOCRUD/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
 		/* UPDATE
 		EmployeeDAO employeeDAO = new EmployeeDAOImpl();
-		Employee employee = new Employee(3,2,"Mahmut","Tuncer",58);
+		Employee employee = new Employee(3,2,"Lorem","Ipsum",58);
 		employeeDAO.update(employee);
 		System.out.println(employee);
 		 */
 
 		/* INSERT
 		EmployeeDAO employeeDAO = new EmployeeDAOImpl();
-		Employee employee = new Employee(0,323,"İbrahim","Tatlıses",54);
+		Employee employee = new Employee(0,323,"John","Dao",54);
 		int result = employeeDAO.insert(employee);
 		System.out.println(employee);
 		 */
